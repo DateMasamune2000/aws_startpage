44651c3eab55d238e07b3a5df84b00f122845172
Varun Girimaji <vgirimaji@gmail.com>
2023-04-07 16:55:43 +0530

    mistake

--- startpage/src/routes/Weather.svelte
+++ startpage/src/routes/Weather.svelte
@@ -1,10 +1,29 @@
 <script>
+    import { subscribe } from "svelte/internal";
     export let Desc = "default"
+	export let coordinate_string = "";
     import { weatherData } from "./stores";
+    import { coordinateData } from "./stores";
+	import { refreshWeather } from "./stores";
+
+	function setCoordinates () {
+		let temp = coordinate_string.split(' ');
+		let temp2 = {
+			"lat": Number(temp[0]),
+			"long": Number(temp[1])
+		};
+
+		$coordinateData = temp2;
+	};
 </script>
 
 <div class="card-text">
 <p>{Desc = "Get weather information"}</p>
-<p>{weatherData.tmp}</p>
+<p>{$weatherData["tmp"]}</p>
+    <!-- <span class="input-group-text" id="basic-addon1">@</span> -->
+<div class="input-group mb-3">
+<button class="input_group_text" id="basic_addon1" on:click={refreshWeather}>@</button>
+<input type="text" on:keypress={setCoordinates} bind:value={coordinate_string} class="form-control" placeholder="<x> <y>" aria-label="Username" aria-describedby="basic-addon1">
+</div>
 </div>
 
--- startpage/src/routes/stores.js
+++ startpage/src/routes/stores.js
@@ -1,12 +1,21 @@
 import { writable } from "svelte/store";
 
-const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=12.97&longitude=77.59&hourly=temperature_2m");
-
-const data = await response.json();
-
-console.log(data);
-
-export const weatherData = {
-	"tmp": data.hourly["temperature_2m"][0],
-	"hum": 50,
+export let coordinateData = {
+	"lat": 0,
+	"long": 0,
 };
+
+export let weatherData = { "tmp": 0, "hum": 0 };
+
+await weatherData;
+
+export const refreshWeather = async () => {
+	const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + coordinateData["lat"] + "&longitude=" + coordinateData["long"] + "&hourly=temperature_2m");
+
+	const data = await response.json();
+
+	weatherData = {
+		"tmp": data.hourly["temperature_2m"][0],
+		"hum": 50,
+	};
+}
