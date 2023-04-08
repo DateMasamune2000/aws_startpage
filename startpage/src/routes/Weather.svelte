85078e42d05d5f9eb6d01f9a56a0af94bfb4673c
Andr0id16 <pracheth.thamankar@gmail.com>
2023-04-08 16:31:51 +0530

    Updated components

--- startpage/src/routes/Weather.svelte
+++ startpage/src/routes/Weather.svelte
@@ -54,15 +54,24 @@
 <div class="row">
 <div class="input-group mb-3">
 <!-- <span class="input-group-text" id="basic-addon1">@</span> -->
-<button class="btn btn-secondary" id="basic_addon1" on:click={rw}>@</button>
+<button class="btn" id="basic_addon1" on:click={rw}>@</button>
 <input type="text" bind:value={coordinate_string} class="form-control" placeholder="<x> <y>" aria-label="Username" aria-describedby="basic-addon1">
 </div>
 </div>
 {:else}
 <div class="row">
-<button class="btn btn-primary" on:click={toggleVisible}>Show location settings</button>
+<button class="btn" on:click={toggleVisible}>Show location settings</button>
 </div>
 {/if}
 </div>
 </div>
+
+<style>
+	button{
+		background-color: #03C988;
+	}
+	button:hover{
+		background-color: #03C988;
+	}
+</style>
 
