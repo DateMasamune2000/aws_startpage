bf7f4f7c81430090d6d507f133437051747c64e9
Varun H Ramurs <73926001+varun872@users.noreply.github.com>
2023-04-26 17:50:09 +0530

    Added Time Card

---
+++ startpage/src/routes/Time.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onDestroy, tick } from 'svelte';
+  
+    let time = new Date();
+    export let Desc = ""
+    const updateTime = () => {
+      time = new Date();
+    };
+  
+    const interval = setInterval(updateTime, 1000);
+  
+    onDestroy(() => {
+      clearInterval(interval);
+    });
+  
+    tick();
+</script>
+  
+<div class="card-text">
+    <p>{Desc = "The current time is: "}</p> 
+    <p>{time.toLocaleTimeString()}</p>  
+</div>
